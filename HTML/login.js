22e7e8c10a91f752a9fcdd8edb4e19cfcc2c010c
NatanFontana <159050979+NatanFontana@users.noreply.github.com>
2024-10-31 17:35:29 -0300

    Add files via upload

--- HTML/login.js
+++ HTML/login.js
@@ -1,6 +1,17 @@
-document.querySelector("form").addEventListener("submit", function(event) {
-    
-        window.location.href = "index.html";
-        event.preventDefault();
+(() => {
+        'use strict';
         
-});
+        const form = document.querySelector('.needs-validation');
+        
+        form.addEventListener('submit', event => {
+            if (!form.checkValidity()) {
+                event.preventDefault();
+                event.stopPropagation();
+            } else {
+                event.preventDefault(); // Previna o envio padrão para permitir o redirecionamento
+                window.location.href = "index.html";
+            }
+            form.classList.add('was-validated');
+        }, false);
+    })();
+    
