bc12cde876fbee2bdca7a2c28bb0deb621fe5484
NatanFontana <159050979+NatanFontana@users.noreply.github.com>
2025-05-26 09:58:35 -0300

    Add files via upload

--- HTML/login.js
+++ HTML/login.js
@@ -1,17 +1,30 @@
-(() => {
-        'use strict';
-        
-        const form = document.querySelector('.needs-validation');
-        
-        form.addEventListener('submit', event => {
-            if (!form.checkValidity()) {
-                event.preventDefault();
-                event.stopPropagation();
-            } else {
-                event.preventDefault(); // Previna o envio padrão para permitir o redirecionamento
-                window.location.href = "index.html";
-            }
-            form.classList.add('was-validated');
-        }, false);
-    })();
-    
+function validar() {
+    var email = document.getElementById("email").value;
+
+    if(email === "") {
+        document.getElementById("valEmail").textContent = "Por favor, preencha este campo";
+        document.getElementById("valEmail").style.color = "red";
+        document.getElementById("valEmail").style.fontFamily = "'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif";
+        document.getElementById("valEmail").style.fontSize = "13px";
+
+        document.getElementById("email").style.borderColor = "red";
+        document.getElementById("email").style.outline = "0px";
+    }
+    else {
+        const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if(regex.test(email)) {
+            document.getElementById("valEmail").textContent = "";
+            document.getElementById("email").style.border = "2px solid black";
+            document.getElementById("email").style.outline = "0px";
+        }
+        else {
+            document.getElementById("valEmail").textContent = "Preencha este campo corretamente";
+            document.getElementById("valEmail").style.color = "red";
+            document.getElementById("valEmail").style.fontFamily = "'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif";
+            document.getElementById("valEmail").style.fontSize = "13px";
+
+            document.getElementById("email").style.borderColor = "red";
+            document.getElementById("email").style.outline = "0px";
+        }
+    }
+}
