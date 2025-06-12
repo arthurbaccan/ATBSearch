4a577a68fb1a80fcd1fca0d24bbaf037beba64f9
NatanFontana <159050979+NatanFontana@users.noreply.github.com>
2025-06-12 08:23:43 -0300

    Add files via upload

--- HTML/login.js
+++ HTML/login.js
@@ -1,5 +1,8 @@
 function validation() {
 
+    // Variavel de validação para pesquisaer o usuario no banco de dados caso a validação esteja correta
+    var validationCheck = 0;
+
     // Validação email ou telefone 
     var emailOrPhone = document.getElementById("emailOrPhone").value;
     if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(emailOrPhone)) {
@@ -10,11 +13,30 @@ function validation() {
         else {
             document.getElementById("valEmailorPhone").style.display = "none";
             document.getElementById("emailOrPhone").style.borderColor = "black";
+            validationCheck++;
         }
     }
     else {
         document.getElementById("valEmailorPhone").style.display = "none";
         document.getElementById("emailOrPhone").style.borderColor = "black";
+        validationCheck++;
     }
 
+    // Validação senha
+    var senha = document.getElementById("senha").value;
+    if (senha.length < 6) {
+        document.getElementById("valSenha").style.display = "flex";
+        document.getElementById("senha").style.borderColor = "red";
+    }
+    else {
+        document.getElementById("valSenha").style.display = "none";
+        document.getElementById("senha").style.borderColor = "black";
+        validationCheck++;
+    }
+
+    // Cadastro do usuario ao enviar o formulario de maneira correta
+    if(validationCheck == 2) {
+        document.getElementById('form-log-usuario').submit();
+        window.location.href = "index.html";
+    }
 }
